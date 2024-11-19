66cfab12a4c22a61c357f20a1fbe28d5bb853599
Yahya <yahyahussein91@gmail.com>
2024-11-19 11:56:31 -0800

    saved url in response object

--- src/test/java/com/yahya/day1/TestSpartanAPI.java
+++ src/test/java/com/yahya/day1/TestSpartanAPI.java
@@ -1,5 +1,7 @@
 package com.yahya.day1;
 
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.*;
 
@@ -8,5 +10,14 @@ public class TestSpartanAPI {
     @Test
     public void testHello(){
         System.out.println("Hello world");
+
+        //Send request to below url and save the response
+        // get method is coming from RestAssured class to send get request to the url defined
+        // store request in response object
+        Response response = get("http://44.211.192.252:8000/api/hello");
+
+
+
+
     }
 }
