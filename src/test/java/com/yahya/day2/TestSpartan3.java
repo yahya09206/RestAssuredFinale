bd04681a7727cd0368ca92ff462b074ad8f653b5
Yahya <yahyahussein91@gmail.com>
2024-11-21 11:17:03 -0800

    test for getting all spartans

--- src/test/java/com/yahya/day2/TestSpartan3.java
+++ src/test/java/com/yahya/day2/TestSpartan3.java
@@ -1,11 +1,37 @@
 package com.yahya.day2;
 
+import io.restassured.response.Response;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
-import static io.restassured.RestAssured.basePath;
-import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.*;
 
 public class TestSpartan3 {
 
+    @BeforeAll
+    public static void setup(){
 
+        baseURI = "http://44.211.192.252:8000";
+        basePath = "/api";
+    }
+
+    @AfterAll
+    public static void tearDown(){
+
+        /**
+         * In order to avoid the static value being accidentally carried over
+         * to a different class when we do a different api
+         * it's better if we set baseURI basePath back to its original value using reset method
+         * RestAssured.reset();
+         */
+        reset();
+    }
+
+    @Test
+    public void testAllSpartans(){
+
+        Response response = get("/spartans");
+        response.prettyPrint();
+    }
 }
