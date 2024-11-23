72498b9f4dbb960a70a86a6e82ffac84cde5b9a6
Yahya <yahyahussein91@gmail.com>
2024-11-23 00:41:48 -0800

    testing with different ids

--- src/test/java/com/yahya/day2/TestSpartan3.java
+++ src/test/java/com/yahya/day2/TestSpartan3.java
@@ -105,6 +105,7 @@ public class TestSpartan3 {
     @Test
     public void testOneSpartanPathParam(){
 
-        Response response = given().pathParam("id", 1).when().get("/spartans/{id}");
+        Response response = given().pathParam("id", 30).when().get("/spartans/{id}");
+        response.prettyPrint();
     }
 }
