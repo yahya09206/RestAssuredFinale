60cacf6f9b05aebc19033ed29a1e9a7afcae3683
Yahya <yahyahussein91@gmail.com>
2024-11-24 00:43:38 -0800

    test for partial update

--- src/test/java/com/yahya/day4/SpartanPutPatch.java
+++ src/test/java/com/yahya/day4/SpartanPutPatch.java
@@ -1,7 +1,11 @@
 package com.yahya.day4;
 
 import com.yahya.utility.SpartanTestBase;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
 public class SpartanPutPatch extends SpartanTestBase {
 
@@ -19,6 +23,44 @@ public class SpartanPutPatch extends SpartanTestBase {
     @Test
     public void testUpdate(){
 
+        String updatedBody = "{\n" +
+                "    \"name\": \"Updated123\",\n" +
+                "    \"gender\": \"Male\",\n" +
+                "    \"phone\": 9999999999\n" +
+                "}";
+
+        given().log().all()
+                .pathParam("id", 93)
+                .contentType(ContentType.JSON)
+                .body(updatedBody)
+                .when().put("spartans/{id}")
+                .then().log().all()
+                .statusCode(equalTo(204));
+
+    }
+
+    /**
+     * PATCH /spartans/{id}
+     * content type is json
+     * body is
+     * {
+     *     "name": "Updated Name",
+     * }
+     * expect status 204
+     */
+
+    @Test
+    public void testPartialUpdate(){
+
+        String updatedBody = "{\"name\": \"Updated Name\"}";
+
+        given().log().all()
+                .pathParam("id", 93)
+                .contentType(ContentType.JSON)
+                .body(updatedBody)
+                .when().patch("spartans/{id}")
+                .then().log().all()
+                .statusCode(equalTo(204));
 
     }
 }
