b5ac3e2a4159fd33249ec777e83481dd827a3070
Yahya <yahyahussein91@gmail.com>
2024-11-24 00:37:00 -0800

    setting up test for updating

---
+++ src/test/java/com/yahya/day4/SpartanPutPatch.java
@@ -0,0 +1,24 @@
+package com.yahya.day4;
+
+import com.yahya.utility.SpartanTestBase;
+import org.junit.jupiter.api.Test;
+
+public class SpartanPutPatch extends SpartanTestBase {
+
+    /**
+     * PUT /spartans/{id}
+     * content type is json
+     * body is
+     * {
+     *     "name": "Updated123",
+     *     "gender": "Male",
+     *     "phone": 9999999999
+     * }
+     * expect status 204
+     */
+    @Test
+    public void testUpdate(){
+
+
+    }
+}
