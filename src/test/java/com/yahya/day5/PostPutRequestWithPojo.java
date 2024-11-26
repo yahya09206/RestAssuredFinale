3c2ef5747b6ccc536aa7813333d72c12d49d78df
Yahya <yahyahussein91@gmail.com>
2024-11-25 21:35:23 -0800

    added instance variables and their getters and setters

--- src/test/java/com/yahya/day5/PostPutRequestWithPojo.java
+++ src/test/java/com/yahya/day5/PostPutRequestWithPojo.java
@@ -11,16 +11,6 @@ public class PostPutRequestWithPojo {
           "gender": "Male",
           "phone": 1231234123
       }
-      Another common way of representing json data is using special purpose Java Object created from A class
-      that have fields matched to json keys
-      and have getters and setters
-      This type of Object, sole purpose is to represent data
-      known as POJO, plain old java object
-      The class to create POJO known as POJO class
-      it's used for creating pojo, just like you would create any other object
-
-      In order to represent a json data with 3 keys name, gender, phone we can create a java class
-      with 3 instance fields with some names
 
       given()
                 .log().all()
--- src/test/java/com/yahya/pojo/Spartan.java
+++ src/test/java/com/yahya/pojo/Spartan.java
@@ -1,4 +1,50 @@
 package com.yahya.pojo;
 
+/*
+    Another common way of representing json data is using special purpose Java Object created from A class
+      that have fields matched to json keys
+      and have getters and setters
+      This type of Object, sole purpose is to represent data
+      known as POJO, plain old java object
+      The class to create POJO known as POJO class
+      it's used for creating pojo, just like you would create any other object
+
+      In order to represent a json data with 3 keys name, gender, phone we can create a java class
+      with 3 instance fields with some names
+
+      only requirements are encapsulated fields such as private instance variables and getters/setters
+     */
 public class Spartan {
+
+    private String name;
+    private String gender;
+    private long phone;
+
+    public Spartan(){
+        // no body required
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(long phone) {
+        this.phone = phone;
+    }
 }
