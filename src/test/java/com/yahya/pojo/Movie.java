34eb99de00acb58bbba2ce6ac7fcc89683f52149
Yahya <yahyahussein91@gmail.com>
2024-11-26 12:14:57 -0800

    set up pojo class

---
+++ src/test/java/com/yahya/pojo/Movie.java
@@ -0,0 +1,61 @@
+package com.yahya.pojo;
+
+public class Movie {
+
+    private String title;
+    private String Year;
+    private String imdbID;
+    private String Type;
+    private String Poster;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getYear() {
+        return Year;
+    }
+
+    public void setYear(String year) {
+        Year = year;
+    }
+
+    public String getImdbID() {
+        return imdbID;
+    }
+
+    public void setImdbID(String imdbID) {
+        this.imdbID = imdbID;
+    }
+
+    public String getType() {
+        return Type;
+    }
+
+    public void setType(String type) {
+        Type = type;
+    }
+
+    public String getPoster() {
+        return Poster;
+    }
+
+    public void setPoster(String poster) {
+        Poster = poster;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "title='" + title + '\'' +
+                ", Year='" + Year + '\'' +
+                ", imdbID='" + imdbID + '\'' +
+                ", Type='" + Type + '\'' +
+                ", Poster='" + Poster + '\'' +
+                '}';
+    }
+}
