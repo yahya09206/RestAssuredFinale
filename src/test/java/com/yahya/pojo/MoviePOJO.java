283c57832ee53fd18f7ec29f5845026e6d3de8bd
Yahya <yahyahussein91@gmail.com>
2024-11-26 12:28:28 -0800

    added correct casing to fields using jsonPropert annotation

--- src/test/java/com/yahya/pojo/MoviePOJO.java
+++ src/test/java/com/yahya/pojo/MoviePOJO.java
@@ -4,11 +4,14 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class MoviePOJO {
 
-    @JsonProperty
+    @JsonProperty("Title")
     private String title;
+    @JsonProperty("Year")
     private String year;
     private String imdbID;
+    @JsonProperty("Type")
     private String type;
+    @JsonProperty("Poster")
     private String poster;
 
     public MoviePOJO() {
