8575d462d4db33ca71aad5634a757aed2f439c93
Yahya <yahyahussein91@gmail.com>
2024-12-28 01:29:08 -0800

    added json property to each field

--- src/test/java/com/yahya/pojo/Jobs.java
+++ src/test/java/com/yahya/pojo/Jobs.java
@@ -1,11 +1,16 @@
 package com.yahya.pojo;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Jobs {
 
-
+    @JsonProperty("job_id")
     private String jobId;
+    @JsonProperty("job_title")
     private String jobTitle;
+    @JsonProperty("min_salary")
     private int minSalary;
+    @JsonProperty("max_salary")
     private int maxSalary;
 
     public String getJobId() {
