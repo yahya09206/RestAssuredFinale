3e9de3cb7674aa20b591b461a19e012fd30de50c
Yahya <yahyahussein91@gmail.com>
2024-12-28 01:51:44 -0800

    jobsv2 class with lombok annotations

--- src/test/java/com/yahya/pojo/Car.java
+++ src/test/java/com/yahya/pojo/Car.java
@@ -4,6 +4,7 @@ import lombok.*;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
 @ToString
 public class Car {
---
+++ src/test/java/com/yahya/pojo/JobsV2.java
@@ -0,0 +1,21 @@
+package com.yahya.pojo;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class JobsV2 {
+
+    @JsonProperty("job_id")
+    private String jobId;
+    @JsonProperty("job_title")
+    private String jobTitle;
+    @JsonProperty("min_salary")
+    private int minSalary;
+    @JsonProperty("max_salary")
+    private int maxSalary;
+}
