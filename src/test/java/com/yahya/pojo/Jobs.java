e2ce36a5389d396550a5764bfb93ece57b8151d2
Yahya <yahyahussein91@gmail.com>
2024-12-28 01:28:09 -0800

    set up fields and getters and setters

--- src/test/java/com/yahya/pojo/Jobs.java
+++ src/test/java/com/yahya/pojo/Jobs.java
@@ -1,4 +1,52 @@
 package com.yahya.pojo;
 
 public class Jobs {
+
+
+    private String jobId;
+    private String jobTitle;
+    private int minSalary;
+    private int maxSalary;
+
+    public String getJobId() {
+        return jobId;
+    }
+
+    public void setJobId(String jobId) {
+        this.jobId = jobId;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public int getMinSalary() {
+        return minSalary;
+    }
+
+    public void setMinSalary(int minSalary) {
+        this.minSalary = minSalary;
+    }
+
+    public int getMaxSalary() {
+        return maxSalary;
+    }
+
+    public void setMaxSalary(int maxSalary) {
+        this.maxSalary = maxSalary;
+    }
+
+    @Override
+    public String toString() {
+        return "Jobs{" +
+                "jobId='" + jobId + '\'' +
+                ", jobTitle='" + jobTitle + '\'' +
+                ", minSalary=" + minSalary +
+                ", maxSalary=" + maxSalary +
+                '}';
+    }
 }
