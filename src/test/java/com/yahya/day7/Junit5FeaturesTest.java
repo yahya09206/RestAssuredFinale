fba0e572284863daa2b8c0ed8ee36cc5f2002a45
Yahya <yahyahussein91@gmail.com>
2024-12-29 09:08:44 -0800

    added values for parameterized test

--- src/test/java/com/yahya/day7/Junit5FeaturesTest.java
+++ src/test/java/com/yahya/day7/Junit5FeaturesTest.java
@@ -2,6 +2,8 @@ package com.yahya.day7;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,11 +16,11 @@ public class Junit5FeaturesTest {
         assertEquals(10, 5+5, "Hey it was not 10");
     }
 
-    @Test
-    public void testMoreThan10(){
+    @ParameterizedTest
+    @ValueSource(ints = {11, 44, 33, 5, 6, 88})
+    public void testMoreThan10(int num ){
         // assuming these numbers are from a coding problem 11, 44, 33, 5, 6, 88
         // assert that all of these numbers are above 10
-        int num = 11;
         assertTrue(num > 10, "Number is not greater than 10");
 
     }
