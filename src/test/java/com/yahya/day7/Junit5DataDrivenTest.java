5227a33094fd5a1c30705cf91ce1cea868c55547
Yahya <yahyahussein91@gmail.com>
2024-12-29 09:19:21 -0800

    done with mmock tests

---
+++ src/test/java/com/yahya/day7/Junit5DataDrivenTest.java
@@ -0,0 +1,20 @@
+package com.yahya.day7;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Junit5DataDrivenTest {
+
+    // Given these Strings of data
+    // "Furkan", "Abbos", "Shazia", "Mohamed"
+    // write a data driven test to check if the length of these strings are greater than 5
+    @ParameterizedTest
+    @ValueSource(strings = {"Furkan", "Abbos", "Shazia", "Mohamed"})
+    public void testNameLength(String names){
+        System.out.println("names = " + names);
+        assertTrue(names.length() > 3);
+    }
+}
