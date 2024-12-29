6d968d92c1379c95d2ddbd4b7423762f71203715
Yahya <yahyahussein91@gmail.com>
2024-12-29 00:57:40 -0800

    initial zipcode test is working

--- src/test/java/com/yahya/day7/ZipcodeAPITest.java
+++ src/test/java/com/yahya/day7/ZipcodeAPITest.java
@@ -2,6 +2,7 @@ package com.yahya.day7;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.*;
 
@@ -19,6 +20,16 @@ public class ZipcodeAPITest {
     public static void tearDown(){
 
         reset();
+    }
+
+    @Test
+    public void testZipToCity(){
+
+        given()
+                .log().all()
+                .pathParam("zip", 98146)
+                .when().get("/{zip}")
+                .then().log().all();
     }
 
 
