43e6a1c92debf06048a4e765b0a4b9e893fa8d58
yahya09206 <yahyahussein91@gmail.com>
2024-12-31 23:58:23 -0800

    skipping header

--- src/test/java/com/yahya/day7/Junit5DataDrivenTest.java
+++ src/test/java/com/yahya/day7/Junit5DataDrivenTest.java
@@ -2,6 +2,8 @@ package com.yahya.day7;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -18,7 +20,13 @@ public class Junit5DataDrivenTest {
         assertTrue(names.length() > 3);
     }
 
+    @ParameterizedTest
+    @CsvFileSource(resources = "/People.csv", numLinesToSkip = 1)
     public void testPerson(String nameParam, String genderParam, long phoneParam){
+
+        System.out.println("nameParam = " + nameParam);
+        System.out.println("genderParam = " + genderParam);
+        System.out.println("phoneParam = " + phoneParam);
 
     }
 }
