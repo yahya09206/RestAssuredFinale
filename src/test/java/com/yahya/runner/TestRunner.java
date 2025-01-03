3b4aa91cbeba4816c2a6276a8f5261596a621c8c
yahya09206 <yahyahussein91@gmail.com>
2025-01-03 10:52:14 -0800

    using runner to execute library login

--- src/test/java/com/yahya/runner/TestRunner.java
+++ src/test/java/com/yahya/runner/TestRunner.java
@@ -1,12 +1,16 @@
 package com.yahya.runner;
 
+import com.yahya.day8.BaseAuthTest;
+import com.yahya.day8.LibraryLoginTest;
+import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 import org.junit.platform.suite.api.SuiteDisplayName;
 
 @Suite
 @SuiteDisplayName("Smoke Test")
-@SelectPackages("com.yahya.day8")
+//@SelectPackages({"com.yahya.day8"})
+@SelectClasses(LibraryLoginTest.class)
 public class TestRunner {
 
 
