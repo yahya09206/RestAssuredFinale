9c5b26799b3deba7f57234af78979db697174206
yahya09206 <yahyahussein91@gmail.com>
2025-01-02 20:50:07 -0800

    setting up test

--- src/test/java/com/yahya/day8/MathTest.java
+++ src/test/java/com/yahya/day8/MathTest.java
@@ -1,6 +1,15 @@
 package com.yahya.day8;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
 public class MathTest {
 
 
+    @ParameterizedTest
+    @CsvFileSource(resources = "/Math.csv", numLinesToSkip = 1)
+    public void testMath(int a, int b, int expected) {
+
+    }
 }
