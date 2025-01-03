eebf9680927a138735742b686d986aaca981ee16
yahya09206 <yahyahussein91@gmail.com>
2025-01-03 11:58:38 -0800

    getting region count

--- src/test/java/com/yahya/tests/day9/API_DB_Test.java
+++ src/test/java/com/yahya/tests/day9/API_DB_Test.java
@@ -5,6 +5,9 @@ import com.yahya.utility.DB_Util;
 import com.yahya.utility.HrORDSTestBase;
 import org.junit.jupiter.api.Test;
 
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
 public class API_DB_Test extends HrORDSTestBase {
 
     @Test
@@ -12,7 +15,18 @@ public class API_DB_Test extends HrORDSTestBase {
 
         DB_Util.runQuery("SELECT * FROM REGIONS");
         DB_Util.displayAllData();
-        DB_Util.destroy();
     }
 
+    @Test
+    public void testRegionsCount(){
+        DB_Util.runQuery("SELECT * FROM REGIONS");
+        int expectedCount = DB_Util.getRowCount();
+
+        given().log().uri().
+        when()
+                .get("/regions").
+                then()
+                    .log().ifValidationFails().statusCode(200)
+                .body("count", is(expectedCount));
+    }
 }
