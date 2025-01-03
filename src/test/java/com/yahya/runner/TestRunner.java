7e7270e04d78d3af8248e1e2c53b5592292488df
yahya09206 <yahyahussein91@gmail.com>
2025-01-03 10:49:51 -0800

    set up runner to run packages at once

---
+++ src/test/java/com/yahya/runner/TestRunner.java
@@ -0,0 +1,13 @@
+package com.yahya.runner;
+
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.SuiteDisplayName;
+
+@Suite
+@SuiteDisplayName("Smoke Test")
+@SelectPackages("com.yahya.day8")
+public class TestRunner {
+
+
+}
