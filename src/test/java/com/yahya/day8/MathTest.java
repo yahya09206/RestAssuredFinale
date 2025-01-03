406eb041d7119369dd1deb4294ab4fd8562f5fbc
yahya09206 <yahyahussein91@gmail.com>
2025-01-02 20:52:48 -0800

    asserting using hamcrest matcher

--- src/test/java/com/yahya/day8/MathTest.java
+++ src/test/java/com/yahya/day8/MathTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+
 public class MathTest {
 
 
@@ -11,5 +14,8 @@ public class MathTest {
     @CsvFileSource(resources = "/Math.csv", numLinesToSkip = 1)
     public void testMath(int a, int b, int expected) {
 
+        System.out.println(a + " + " + b + " should be " + expected);
+        // assert using hamcrest matcher
+        assertThat(a + b, equalTo(expected));
     }
 }
