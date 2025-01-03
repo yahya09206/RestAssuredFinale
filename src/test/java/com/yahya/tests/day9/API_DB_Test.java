ca4655691144698066bc3b78c3dd7086ad74ef9b
yahya09206 <yahyahussein91@gmail.com>
2025-01-03 14:44:29 -0800

    verifying for single region id and name

--- src/test/java/com/yahya/tests/day9/API_DB_Test.java
+++ src/test/java/com/yahya/tests/day9/API_DB_Test.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import java.util.Map;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
@@ -33,5 +35,29 @@ public class API_DB_Test extends HrORDSTestBase {
                     .log().ifValidationFails().statusCode(200)
                 .body("count", is(expectedCount))
                 .body("items", hasSize(expectedCount));
+    }
+
+    /**
+     * Send request to GET /regions/{region_id} region_id = 1
+     * prepare expected result from the database by running
+     * SELECT * FROM REGIONS WHERE REGION_ID = 1
+     * Save the result of query as Map
+     * then verify the region_id and region_name match between api and db response
+     */
+    @Test
+    public void testSingleRegion(){
+
+        DB_Util.runQuery("SELECT * FROM REGIONS WHERE REGION_ID = 1");
+        Map<String, String> dbResultMap = DB_Util.getRowMap(1);
+        System.out.println("dbResultMap = " + dbResultMap);
+
+        int expectedCount = Integer.parseInt(dbResultMap.get("REGION_ID"));
+        String expectedRegionName = dbResultMap.get("REGION_NAME");
+
+        given().param("region_id", 1).log().uri()
+                .when().get("/regions/{region_id}")
+                .then().log().all().statusCode(200);
+
+
     }
 }
