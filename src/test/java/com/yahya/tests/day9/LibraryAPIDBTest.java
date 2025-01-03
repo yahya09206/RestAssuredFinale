6c25174a0f80a50dc2fe8ed848d5a8b4d42db4ae
yahya09206 <yahyahussein91@gmail.com>
2025-01-03 15:52:06 -0800

    getting expected book count

--- src/test/java/com/yahya/tests/day9/LibraryAPIDBTest.java
+++ src/test/java/com/yahya/tests/day9/LibraryAPIDBTest.java
@@ -2,6 +2,7 @@ package com.yahya.tests.day9;
 
 import com.yahya.utility.DB_Util;
 import com.yahya.utility.LibraryAPI_BaseTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class LibraryAPIDBTest extends LibraryAPI_BaseTest {
@@ -11,4 +12,18 @@ public class LibraryAPIDBTest extends LibraryAPI_BaseTest {
         DB_Util.runQuery("SELECT * FROM books");
         DB_Util.displayAllData();
     }
+
+    @DisplayName("Test GET /Dashboard response matches with database")
+    @Test
+    public void testDashboardStatsMatchDB(){
+
+        DB_Util.runQuery("SELECT count(*) FROM books");
+        DB_Util.displayAllData();
+        String expectedBookCount = DB_Util.getCellValue(1, 1);
+
+        //DB_Util.runQuery("SELECT COUNT(*) FROM USERS");
+
+        //DB_Util.runQuery("SELECT COUNT(*) FROM BOOK_BORROW WHERE IS_RETURNED = 0");
+    }
+
 }
