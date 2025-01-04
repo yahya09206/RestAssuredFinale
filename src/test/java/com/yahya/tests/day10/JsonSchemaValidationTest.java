927fb7c61ca03af7436e02f870136cc725fbf911
Yahya <yahyahussein91@gmail.com>
2025-01-04 01:27:52 -0800

    added import for schema validator

--- src/test/java/com/yahya/tests/day10/JsonSchemaValidationTest.java
+++ src/test/java/com/yahya/tests/day10/JsonSchemaValidationTest.java
@@ -1,6 +1,21 @@
 package com.yahya.tests.day10;
 
-public class JsonSchemaValidationTest {
+import com.yahya.utility.SpartanTestBase;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
+import static io.restassured.RestAssured.*;
+import static io.restassured.module.jsv.JsonSchemaValidator.*;
 
+public class JsonSchemaValidationTest extends SpartanTestBase {
+
+    @DisplayName("Testing GET /spartan{id} response schema")
+    @Test
+    public void testSingleSpartanSchema(){
+
+        given().pathParam("id", 93).log().uri()
+                .when().get("/spartans/{id}")
+                .then().log().all().statusCode(200)
+                .body();
+    }
 }
